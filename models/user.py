5950b1e0eb8a168d6b2dfef8df8899268b684ab1
c3041660 <c3041660@ncl.ac.uk>
2024-03-02 21:04:36 +0000

    Switch name to email

--- models/user.py
+++ models/user.py
@@ -4,11 +4,11 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
     password = db.Column(db.String(255))
     role = db.Column(db.String(100))
 
-    def __init__(self, username, password, role):
-        self.username = username
+    def __init__(self, email, password, role):
+        self.email = email
         self.password = password
         self.role = role
