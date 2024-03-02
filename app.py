c03c78ab41ccd422be443d8fe75d23f9948ba1ec
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-03-02 20:05:33 +0000

    feat: add error handling and requirements

--- app.py
+++ app.py
@@ -8,5 +8,30 @@ def index():
     return render_template('main/index.html')
 
 
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template("errors/400.html"), 400
+
+
+@app.errorhandler(403)
+def access_denied(error):
+    return render_template("errors/403.html"), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("errors/500.html"), 500
+
+
+@app.errorhandler(503)
+def service_unavailable(error):
+    return render_template("errors/503.html"), 503
+
+
 if __name__ == '__main__':
     app.run()
