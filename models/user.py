951652a36fdfdb302c82327ddbdfbb08de4cf7eb
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-03-02 21:28:37 +0000

    patch: add hashing and make fields not nullable

--- models/user.py
+++ models/user.py
@@ -1,14 +1,26 @@
+import bcrypt
+
 from app import db, app
+
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100))
-    password = db.Column(db.String(255))
-    role = db.Column(db.String(100))
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(100), nullable=False, default='user')
 
     def __init__(self, email, password, role):
         self.email = email
-        self.password = password
+        self.password = self.encrypt_password(password)
         self.role = role
+
+    # password hashing
+    @staticmethod
+    def encrypt_password(password):
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    # password verification
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password)
