13ed4468bb7be07ebc6dff764e36b5f7c007da0a
tinyTim567 <tmck911@gmail.com>
2024-03-03 08:49:12 +0000

    Fix location_name thing

--- utils/import_hack_athons_uk.py
+++ utils/import_hack_athons_uk.py
@@ -34,15 +34,24 @@ if __name__ == '__main__':
 			end_day = int(re.sub('\D', '', hackathon['when'].split("-")[1].split(" ")[0]) if "-" in hackathon['when'] else start_day)
 			start_month = (int(months.index(hackathon['when'].split("-")[0].split(" ")[-1].lower())) + 1) if hackathon['when'].split("-")[0].split(" ")[-1].lower() in months else end_month
 			digital = hackathon.get('digital') or False
+
+			if hackathon.get('location'):
+				location_name = hackathon.get('location')
+			elif digital:
+				location_name = "Online"
+			else:
+				location_name = None
+
 			nh = {
-				'name' : hackathon['name'],
-				'url' : hackathon.get('website') or None,
-				'start_date' : datetime.datetime(year, start_month, start_day).isoformat(),
-				'location_name' : hackathon.get('location') or "Online" if digital else None,
-				'attendees' : (hackathon['attendees'] if hackathon['attendees'] != 'TBC' else None),
-				'end_date' : datetime.datetime(year, end_month, end_day).isoformat(),
-				'digital_only' : digital
+				'name': hackathon['name'],
+				'url': hackathon.get('website') or None,
+				'start_date': datetime.datetime(year, start_month, start_day).isoformat(),
+				'location_name': location_name,
+				'attendees': (hackathon['attendees'] if hackathon['attendees'] != 'TBC' else None),
+				'end_date': datetime.datetime(year, end_month, end_day).isoformat(),
+				'digital_only': digital
 			}
+
 			h.append(nh)
 	f = open('../events_hack.athons.uk.json' , 'w')
 	f.write(json.dumps(h))
