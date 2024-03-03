27648887ce908a1b7edd3ec1cde8cb9ca5af3594
GeorgeTsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-03-03 04:35:43 +0000

    feat: store sponsor logo image

--- users/views.py
+++ users/views.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
-from flask_login import login_user, logout_user, login_required, current_user
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, login_required
 from users.forms import LoginForm, UserRegistrationForm, SponsorRegistrationForm
 from models.user import User
-from app import db
+from app import db, app
+import os
+from werkzeug.utils import secure_filename
 
-users_blueprint = Blueprint('users', __name__, template_folder='templates')
+UPLOAD_FOLDER = os.path.join(app.root_path, 'static/img/')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+users_blueprint = Blueprint('users', __name__, template_folder='templates', static_folder='static')
 
 
 @users_blueprint.route('/login', methods=['GET', 'POST'])
@@ -38,11 +45,39 @@ def profile():
     return render_template('users/profile.html') # TODO: replace with actual code
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def save_picture(form):
+    if request.method == 'POST':
+        # if user does not select file
+        if 'picture' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['picture']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # use company name + extension as filename
+            filename = secure_filename(form.company.data.lower() + '.' + file.filename.rsplit('.', 1)[1].lower())
+            print(file)
+            with open(app.config['UPLOAD_FOLDER'] + filename, 'wb') as f:
+                f.write(file.read())
+
+
 def register_user(form, role):
+    # check if email already exists
     user = User.query.filter_by(email=form.email.data).first()
     if user:
         flash('Email address already exists.')
         return render_template('users/register.html', form=form)
+
+    save_picture(form)
+
     new_user = User(email=form.email.data,
                     password=form.password.data,
                     role=role)
