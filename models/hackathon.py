5653bbaee50101dac8c5457f1dbd04b34f397caf
CaptureTheFrog <capturethefrog@proton.me>
2024-03-03 10:46:37 +0000

    Remove institution from db

--- models/hackathon.py
+++ models/hackathon.py
@@ -15,11 +15,10 @@ class Hackathon(db.Model):
     phone = db.Column(db.String(100))
     postal_address = db.Column(db.String(100))
 
-    def __init__(self, title, start_date, end_date, institution, location, event_type, description, email, phone, postal_address, organisers):
+    def __init__(self, title, start_date, end_date, location, event_type, description, email, phone, postal_address, organisers):
         self.title = title
         self.start_date = start_date
         self.end_date = end_date
-        self.institution = institution
         self.location = location
         self.event_type = event_type
         self.description = description
--- utils/import_hackathons_to_db.py
+++ utils/import_hackathons_to_db.py
@@ -12,13 +12,11 @@ def import_hackathons_to_db(db: SQLAlchemy):
                               datetime.datetime.fromisoformat(h['start_date']),
                               datetime.datetime.fromisoformat(h['end_date']),
                               h['location_name'],
-                              '',
                               'Hackathon',
                               '',
                               '',
                               '',
-                              '',
-                              None
+                              ''
                               )
         db.session.add(hackathon)
     db.session.commit()
